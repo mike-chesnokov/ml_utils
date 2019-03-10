d06bd0631f5ee27841c96d185dfbe0efea2f98b2
mike-chesnokov <chesnokov_mike@mail.ru>
2019-03-10 13:28:41 +0300

    + lgb params ! style

--- lightgbm_utils.py
+++ lightgbm_utils.py
@@ -4,21 +4,47 @@ import gc
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
+from sklearn.model_selection import KFold
 
 from metrics_utils import rmse, rmse_lgb
 
+kf = KFold(n_splits=5, random_state=7, shuffle=False)
+lgb_regression_params = {
+    'boosting': 'gbdt',
+    'objective': 'regression',
+    'metric': 'rmse',
+    'boost_from_average': False,
+    'is_training_metric': False,
+    'seed': 77,
+    'learning_rate': 0.01,
+    #'max_depth': 5,
+    'min_data_in_leaf': 200,
+    #'min_gain_to_split': 0.001,
+    #'min_sum_hessian_in_leaf': 120.,
+    'save_binary': True,
+    'num_leaves': 31,
+    #'max_bin': 32,
+    'feature_fraction': 0.8,
+    'bagging_fraction': 0.8,
+    'bagging_freq': 10,
+    'lambda_l2': 1.,
+    'lambda_l1': 1.,
+    'num_threads': 8,
+    'verbosity': -1
+    }
 
-def lgb_cv(X_train, 
-           y_train,
-           X_test,
-           lgb_params,
-           cv_schema,
-           num_trees=1000,
-           early_stopping=200,
-           verbose_eval=None,
-           print_progress=False):
+
+def lgb_cv_regression(X_train,
+                      y_train,
+                      X_test,
+                      lgb_params,
+                      cv_schema,
+                      num_trees=1000,
+                      early_stopping=200,
+                      verbose_eval=None,
+                      print_progress=False):
     """
-    Calculate LightGBM cross-validation by cv_schema
+    Calculate LightGBM cross-validation by cv_schema for regression task (metric - RMSE)
     
     params:
         X_train: 2d array, train data
@@ -37,10 +63,8 @@ def lgb_cv(X_train,
         RMSE std over folds
         mean of best LightGBM iteration over folds
     """
-    # make predictions on test dataset
-    if X_test:
-        lgb_preds = []
-    
+
+    lgb_preds = []
     rmses = []
     best_iterations = []
 
@@ -55,12 +79,12 @@ def lgb_cv(X_train,
         y_train_, y_valid_ = y_train.values[train_index], y_train.values[valid_index]
 
         lgb_train = lgb.Dataset(X_train_, y_train_) 
-        lgb_valid = lgb.Dataset(X_valid_, y_valid_, reference = lgb_train)
+        lgb_valid = lgb.Dataset(X_valid_, y_valid_, reference=lgb_train)
 
         lgb_model = lgb.train(lgb_params, lgb_train, num_trees,  
-                              valid_sets = [lgb_valid], 
+                              valid_sets=[lgb_valid],
                               valid_names=['valid'],
-                              feval = rmse_lgb, 
+                              feval=rmse_lgb,
                               verbose_eval=verbose_eval, 
                               early_stopping_rounds=early_stopping,
                               feature_name=list(X_train.columns))
@@ -71,10 +95,8 @@ def lgb_cv(X_train,
         if print_progress:
             print('Fold trained...')
 
-        # make predictions on test and valid datasets
-        if X_test:
-            lgb_pred = lgb_model.predict(X_test,  num_iteration = lgb_model.best_iteration)
-        y_valid_pred = lgb_model.predict(X_valid_,  num_iteration = lgb_model.best_iteration)
+        # make predictions on valid dataset
+        y_valid_pred = lgb_model.predict(X_valid_,  num_iteration=lgb_model.best_iteration)
 
         best_iterations.append(lgb_model.best_iteration)
         rmse_current = rmse(y_valid_, y_valid_pred)
@@ -82,7 +104,9 @@ def lgb_cv(X_train,
         if print_progress:
             print('RMSE  valid',  round(rmse_current, 6))
 
+        # make predictions on test dataset
         if X_test:
+            lgb_pred = lgb_model.predict(X_test, num_iteration=lgb_model.best_iteration)
             lgb_preds.append(lgb_pred)
         y_valid_preds[valid_index] = y_valid_pred
 
@@ -99,4 +123,3 @@ def lgb_cv(X_train,
         return np.mean(lgb_preds, axis=0), rmse(y_train, y_valid_preds), np.mean(rmses), np.std(rmses), np.mean(best_iterations)
     else:
         return rmse(y_train, y_valid_preds), np.mean(rmses), np.std(rmses), np.mean(best_iterations)
-    
